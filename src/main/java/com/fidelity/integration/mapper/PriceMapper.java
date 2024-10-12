b472681fc2342e789ab4a717bc13d4c7d3a15996
FuryACE007 <sudhanshuvshekhar@gmail.com>
2024-10-12 17:02:07 +0530

    updated table

--- src/main/java/com/fidelity/integration/mapper/PriceMapper.java
+++ src/main/java/com/fidelity/integration/mapper/PriceMapper.java
@@ -8,6 +8,6 @@ import java.math.BigDecimal;
 @Mapper
 public interface PriceMapper {
 
-    @Select("SELECT bidPrice FROM Price WHERE instrumentId = #{instrumentId} ORDER BY priceTimestamp DESC LIMIT 1")
+    @Select("SELECT bidPrice FROM (SELECT bidPrice FROM Price WHERE instrumentId = #{instrumentId} ORDER BY timestamp DESC) WHERE ROWNUM = 1")
     BigDecimal getCurrentBidPrice(String instrumentId);
 }
